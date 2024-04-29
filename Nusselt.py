82d88dd3fc01fb0a78f9a9bf3568208e713153d1
nader nofal <naderbluastrike@gmail.com>
2024-04-29 21:58:04 +0300

    NusseltNumber

--- Nusselt.py
+++ Nusselt.py
@@ -14,4 +14,30 @@ class NusseltNumber(object):
             Nu=(0.037*(rynolds[0]**(4/5))-871.3)*(rynolds[1]**(1/3))
         return Nu
     
+    def forced_constant_heat_flux_plate(self,u_infinity:float,length): 
     
+        rynolds=self.rynolds.Calculate(u_infinity=u_infinity,length=length)
+        Nu=0.680*(rynolds[0]**0.5)*(rynolds[1]**(1/3))
+        return Nu
+    
+    def cylinder_in_cross_flow(self,u_infinity:float,diameter):
+        rynolds=self.rynolds.Calculate(u_infinity=u_infinity,length=diameter)
+
+        if 0.4<rynolds[0] & rynolds[0]<4: # Table 7.2 Page 458
+            C=0.989
+            M=0.330
+        elif 4<=rynolds[0] & rynolds[0]<40:
+            C=0.911
+            M=0.385
+        elif 40<=rynolds[0] & rynolds[0]<4000:
+            C=0.683
+            M=0.466
+        elif 4000<=rynolds[0] & rynolds[0]<40000:
+            C=0.193
+            M=0.618
+        elif 40000<=rynolds[0] & rynolds[0]<400000:
+            C=0.027
+            M=0.805
+        Nu=C*(rynolds[0]**M)*(rynolds[1]**(1/3))
+        return Nu
+        
--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 I have made this python based heat transfer project to help calculate Convection Heat Transfer Coefficient For various cases like:<br>
     -  flat plate subjected to free convection either horizantal or vertical.<br>
     -  flat plate subjected to forced convection by gas (turbulent, laminar or mixed).<br>
+    -  flat plate subjected to forced convection by gas with constant heat flux (turbulent, laminar or mixed).<br>
+    -  cylender subjected to a cross forced convection.<br>
     
